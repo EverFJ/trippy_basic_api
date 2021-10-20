39b0a42fc3cb3ecb18dac799b407f64613beb8af
Francois Janson <jansonfrancois@gmail.com>
2021-10-20 12:00:21 +0200

    login jwt handler

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,13 @@
 const jwt = require("jsonwebtoken")
+const JWT_SECRET = process.env.JWT_SECRET || 'supercledechiffrementsuperlongue'
+
 
 const handleLogin = (req, res) => {
-
+    console.log(`JWT_SECRET`, JWT_SECRET)
+    const token = jwt.sign({
+        username: req.body.username
+    }, JWT_SECRET)
+    res.json(token)
 }
 
 module.exports = {
