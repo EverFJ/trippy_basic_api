5a25f51234f4425db041e2c2193994e92f158bf3
Francois Janson <jansonfrancois@gmail.com>
2021-10-20 12:01:46 +0200

    cleaning

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,7 +3,6 @@ const JWT_SECRET = process.env.JWT_SECRET || 'supercledechiffrementsuperlongue'
 
 
 const handleLogin = (req, res) => {
-    console.log(`JWT_SECRET`, JWT_SECRET)
     const token = jwt.sign({
         username: req.body.username
     }, JWT_SECRET)
