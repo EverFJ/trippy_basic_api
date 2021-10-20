4dcd99e5fa1766b4729ca5b78320dcfeb23b6a5a
Francois Janson <jansonfrancois@gmail.com>
2021-10-20 09:58:11 +0200

    env

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require("express")
 const app = express()
-
+require("dotenv").config()
+const {
+    PORT
+} = process.env
 const hotels = ["Hilton", "Ritz", "Trianon Palace"]
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${port}`)
+})
