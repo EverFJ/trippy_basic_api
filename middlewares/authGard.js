02305dd6a0983d5f391279dc6879146c99e05daa
Francois Janson <jansonfrancois@gmail.com>
2021-10-22 14:24:14 +0200

    authGard middleware

--- middlewares/authGard.js
+++ middlewares/authGard.js
@@ -1,21 +1,22 @@
 const jwt = require("jsonwebtoken")
 const JWT_SECRET = process.env.JWT_SECRET
+const mongoose = require("mongoose")
+const Users = require("../models/Users")
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.cookie.split("=")[1]
-        const decodedToken = jwt.verify(token, JWT_SECRET)
-        console.log(`decodedToken`, decodedToken)
-        const username = decodedToken.username
 
-
-
-        // next()
-    } catch {
-
-        res.status(401).json({
-            error: "You're not allowed to see this"
+    const token = req.headers.cookie.split("=")[1]
+    const decodedToken = jwt.verify(token, JWT_SECRET)
+    console.log(`decodedToken`, decodedToken)
+    const username = decodedToken.username
+    Users.findOne({
+            username: username
         })
-
-    }
+        .then(user => next())
+        .catch(err => {
+            res.status(401).json({
+                message: "You're not allowed to see this",
+                error: err
+            })
+        })
 }
